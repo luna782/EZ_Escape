857e5b8ae5813a67133eb283a2c098ed69cfaa8e
Paul Luna <paullunapal@gmail.com>
2023-04-01 20:29:12 -0500

    Controller Package: Screen1LoginController implementation started.  - Added 2 import statements  - Added 2 private variables  - Added constructor method  - Added onClick method   - Note: Still needs "getInputFromTextBox" method  - Added setPassword method

--- app/src/main/java/com/example/ez_escape/controller/Screen1LoginController.java
+++ app/src/main/java/com/example/ez_escape/controller/Screen1LoginController.java
@@ -8,5 +8,32 @@ package com.example.ez_escape.controller;
  * Spring 2023
  */
 
-public class Screen1LoginController {
+import android.view.View;
+import android.widget.Toast;
+
+public class Screen1LoginController implements View.OnClickListener{
+    private String message;
+    private String password;
+    //Method will be called on creation of application;
+    public Screen1LoginController(String password){
+        this.message = "Wrong Password";
+        this.password = password;
+    }
+    public void onClick(View view) {
+        //Only logic for the onclick method if statement; Implement actual function calls later for getting text
+        if (password.equals(getInputFromTextBox)) {
+            //Change Activity to Screen 2
+        } else {
+            //Tell User that the password is incorrect
+            //Currently using Toast to do this
+            Toast t = Toast.makeText(view.getContext(), this.message, Toast.LENGTH_SHORT);
+            t.show();
+        }
+
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
