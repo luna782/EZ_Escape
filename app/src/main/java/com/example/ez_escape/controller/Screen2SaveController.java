91b57e6ee547d2afaca3123aacefd4d12638a7fe
Paul Luna <paullunapal@gmail.com>
2023-04-01 19:24:58 -0500

    Controller Package: Screen3ClosePopUp added. Screen3NextMonthController added. Screen3PreviousMonthController added. Screen3ViewAlertsController added. Screen3ViewEditDeleteAlertController added. Screen4SubmitNewPin added.
    Screen1LoginController Java doc formatting added.
    Screen2ClearController Java doc formatting added.
    Screen2SaveController Java doc formatting added.
    Screen3ClosePopUp Java doc formatting added.
    Screen3NextMonthController Java doc formatting added.
    Screen3PreviousMonthController Java doc formatting added.
    Screen3ViewAlertsController Java doc formatting added.
    Screen3ViewEditDeleteAlertController Java doc formatting added.
    Screen4SubmitNewPin Java doc formatting added.
    
    Model Package:
    temp.txt added as placeholders.
    
    Screen Package:
    temp.txt added as placeholders.
    
    Note: All Controller file currently do not have any content in the classes themselves.

--- app/src/main/java/com/example/ez_escape/controller/Screen2SaveController.java
+++ app/src/main/java/com/example/ez_escape/controller/Screen2SaveController.java
@@ -1,4 +1,12 @@
 package com.example.ez_escape.controller;
 
+/**
+ * Description of class.
+ *
+ * @author Name (abc123)
+ * UTSA CS 3443 - Final Project
+ * Spring 2023
+ */
+
 public class Screen2SaveController {
 }
