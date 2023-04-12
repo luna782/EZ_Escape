8e2804cf65cba9f2ae3e3302654c96b7494f4698
Vic <victor.h.mtz@hotmail.com>
2023-04-12 16:12:02 -0500

    created xml file for second screen

--- app/src/main/java/com/example/ez_escape/AddNewAlertActivity.java
+++ app/src/main/java/com/example/ez_escape/AddNewAlertActivity.java
@@ -10,5 +10,7 @@ public class AddNewAlertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_alert);
+
+
     }
 }
--- app/src/main/java/com/example/ez_escape/controller/Screen2ClearController.java
+++ app/src/main/java/com/example/ez_escape/controller/Screen2ClearController.java
@@ -1,5 +1,10 @@
 package com.example.ez_escape.controller;
 
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.ez_escape.AddNewAlertActivity;
+
 /**
  * Description of class.
  *
@@ -8,5 +13,21 @@ package com.example.ez_escape.controller;
  * Spring 2023
  */
 
-public class Screen2ClearController{
+public class Screen2ClearController implements View.OnClickListener{
+    EditText date;
+    EditText time;
+    EditText sender;
+    EditText message;
+    AddNewAlertActivity addNewAlertActivity;
+
+
+    public Screen2ClearController(AddNewAlertActivity addNewAlertActivity){
+        this.addNewAlertActivity = addNewAlertActivity;
+        date = findViewById(addNewAlertActivity.R.id.date);
+    }
+    @Override
+    public void onClick(View view) {
+        textView.setText(" ");
+    }
+
 }
