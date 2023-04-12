062fb1eec1e7d5e408727ffc4729965313f989ca
Ramin Mohammadi <rammoh5346@gmail.com>
2023-04-12 15:01:43 -0500

    created editviewdelete activity

---
+++ app/src/main/java/com/example/ez_escape/EditViewDeleteAlert.java
@@ -0,0 +1,14 @@
+package com.example.ez_escape;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditViewDeleteAlert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_view_delete_alert);
+    }
+}
