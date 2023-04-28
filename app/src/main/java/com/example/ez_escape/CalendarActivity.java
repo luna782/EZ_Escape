d8edf950b1d61ebb09738f8c213c3046566edbff
Paul Luna <paullunapal@gmail.com>
2023-04-28 14:54:52 -0500

    XML file changes: activity_calendar.xml
    Java changes:
    CalendarActivity.java
    ViewAlertActivity.java

--- app/src/main/java/com/example/ez_escape/CalendarActivity.java
+++ app/src/main/java/com/example/ez_escape/CalendarActivity.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.CalendarView;
 
+import com.example.ez_escape.controller.CalendarController;
 import com.example.ez_escape.controller.NewAlertController;
 import com.example.ez_escape.controller.Screen3ViewAlertsController;
+import com.example.ez_escape.controller.SettingsController;
 
 public class CalendarActivity extends AppCompatActivity {
 
@@ -15,6 +17,9 @@ public class CalendarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        //Create Calendar controller
+        //CalendarController calendarCtrl = new CalendarController(this);
 
         //Create CalendarView object
         CalendarView calendar = findViewById(R.id.calendar);
@@ -30,7 +35,7 @@ public class CalendarActivity extends AppCompatActivity {
 
         //Button that sends user to newAlertActivity Screen
         Button settingsButton = findViewById(R.id.settings_button);
-        newAlert.setOnClickListener(new NewAlertController(this) );
+        settingsButton.setOnClickListener(new SettingsController(this) );
 
     }
 }
--- app/src/main/java/com/example/ez_escape/ViewAlertActivity.java
+++ app/src/main/java/com/example/ez_escape/ViewAlertActivity.java
@@ -13,6 +13,7 @@ public class ViewAlertActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Change activity!
         setContentView(R.layout.activity_calendar);
 
 
