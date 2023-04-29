828ee050aae6614b2d1ceca1a4bc108c1a418aab
Vic <victor.h.mtz@hotmail.com>
2023-04-29 17:19:20 -0500

    getAlerts() returns an arraylsit of alerts

--- app/src/main/java/com/example/ez_escape/CalendarActivity.java
+++ app/src/main/java/com/example/ez_escape/CalendarActivity.java
@@ -22,18 +22,18 @@ public class CalendarActivity extends AppCompatActivity {
         //CalendarController calendarCtrl = new CalendarController(this);
 
         //Create CalendarView object
-        CalendarView calendar = findViewById(R.id.calendar);
+//        CalendarView calendar = findViewById(R.id.calendar);
 
         //Button that sends user to viewAlertActivity Screen
         Button viewAlert = findViewById(R.id.view_alert_button);
-        viewAlert.setOnClickListener(new Screen3ViewAlertsController(this,calendar) );
+//        viewAlert.setOnClickListener(new Screen3ViewAlertsController(this,calendar) );
 
 
         //Button that sends user to newAlertActivity Screen
         Button newAlert = findViewById(R.id.new_alert_button);
         newAlert.setOnClickListener(new NewAlertController(this) );
 
-        //Button that sends user to newAlertActivity Screen
+        //Button that sends user to Settings Screen
         Button settingsButton = findViewById(R.id.settings_button);
         settingsButton.setOnClickListener(new SettingsController(this) );
 
